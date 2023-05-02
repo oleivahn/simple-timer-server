6813ccc257224c48523ccc37a1ee2e2676ac792f
Omar Leiva <oleivahn@gmail.com>
2023-05-02 00:54:08 -0400

    MongoDB Client installed and working

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-// create a server and listen for connections on port 5000
 const express = require("express");
-const mongoose = require("mongoose");
+// MONGODB
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = 5000;
 
 const cors = require("cors");
+
+// // MONGOOSE
+const uri =
+  "mongodb+srv://leivao:TgkzKJz5VVhR0LsH@simple-workouts-cluster.ppkmo2t.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 // // connect to the database
 // const db = mongoose.connect(
@@ -32,4 +46,22 @@ app.use(
 // app.get("/api", (req, res) => res.send("Hello World!"));
 app.get("/api", (req, res) => res.send(data));
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// TITLE: CONNECT TO MONGODB FIRST AND THE START THE SERVER
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // START THE SERVER
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
