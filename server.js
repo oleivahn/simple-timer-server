960f849abd153ce303de4dd1692c68dfde04ef82
Omar Leiva <oleivahn@gmail.com>
2023-05-01 23:24:31 -0400

    Initial commit basic server. Me voy a probar pasar de 50 character

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 // create a server and listen for connections on port 5000
 const express = require("express");
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const port = 5000;
 
+const data = require("./data.json");
+
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 // app.get("/api", (req, res) => res.send("Hello World!"));
-app.get("/api", (req, res) => res.json({ msg: "Hello World!" }));
+app.get("/api", (req, res) => res.send(data));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
